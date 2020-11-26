24282e11049e0289f82fea3d28cbc528af6b331c
chinelo.okoli <chinelo.okoli@gmail.com>
2020-11-26 15:05:46 +0000

    Initial Project Commit

---
+++ src/main/java/guru/springframework/chokpetclinic/ChokPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.chokpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChokPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChokPetClinicApplication.class, args);
+    }
+
+}
