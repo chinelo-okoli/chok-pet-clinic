8357cbdaa1e1f6a05536a9784f75479cf3823abb
chinelo.okoli <chinelo.okoli@gmail.com>
2020-11-29 22:30:30 +0000

    Refactored Map Impl to abstract ID generation. Closes #24.

--- pet-clinic-data/src/main/java/guru/springframework/chokpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/chokpetclinic/services/map/AbstractMapService.java
@@ -8,8 +8,8 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T extends BaseEntity<ID>, ID> implements CrudService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity<Long>, ID extends Long> implements CrudService<T, ID> {
+    protected Map<Long, T> map = new HashMap<>();
 
     @Override
     public Set<T> findAll() {
@@ -23,6 +23,11 @@ public abstract class AbstractMapService<T extends BaseEntity<ID>, ID> implement
 
     @Override
     public T save(T object) {
+        if(object != null && object.getId() == null) {
+            object.setId(getNextId());
+        } else if(object == null) {
+            throw new RuntimeException("Object cannot be null");
+        }
         map.put(object.getId(), object);
         return object;
     }
@@ -35,5 +40,10 @@ public abstract class AbstractMapService<T extends BaseEntity<ID>, ID> implement
     @Override
     public void deleteById(ID id) {
         map.remove(id);
+    }
+
+    private Long getNextId() {
+        Long maxKey = map.isEmpty() ? 0L : map.keySet().stream().reduce(Long::max).get();
+        return maxKey + 1L;
     }
 }
